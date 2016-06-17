e875f4759c1977ded82268226c3a3df7dd93a1d1
zhuming <405557524@qq.com>
2016-06-17 15:39:11 +0800

    第一次提交

--- ScrollProject/app/src/main/java/com/example/soul/scrollproject/MainActivity.java
+++ ScrollProject/app/src/main/java/com/example/soul/scrollproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.soul.scrollproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View viewById = findViewById(R.id.tv_fads);
     }
 }
